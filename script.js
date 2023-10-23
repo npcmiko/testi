b8621e75bf8b45575a32e26bfb8d42e571c331be
Mikooooo <miko.huitila@edu.hyria.fi>
2023-10-23 13:53:14 +0300

    tehtava

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1.
+const str = "nimi";
+const modStr = str[0].toUpperCase() + str.slice(1);
+console.log(modStr);
+
+// 2.
+const str2 = "kala, hevonen, kana";
+const text1 = str2.substr(0,13);
+console.log(text1);
+
+// 3.
+const str3 = "rovaniemi";
+const letter = str3.charAt(4)
+console.log(letter);
+
+// 4.
+const str4 = "Yhden yön tarinoita";
+const result = str4.replace("yön", "päivän");
+console.log(result);
+
+// 5.
+function getLastLetters(str, a) {
+    return str.slice(-a);
+}
+
+const exampleString = 'HelloWorld';
+const a = 3;
+const result2 = getLastLetters(exampleString, a);
+console.log(result2);
+
+// 6.
+for (let i=100; i >= 0; i--) {
+    if (i % 7 === 0) {
+        console.log("i = ", i)
+    } 
+}
